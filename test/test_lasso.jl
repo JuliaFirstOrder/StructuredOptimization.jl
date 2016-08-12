0705bd24679e7395d8f154142fd3b5eefda981b6
Lorenzo Stella <lorenzostella@gmail.com>
2016-08-12 14:36:36 +0200

    updated tests

--- test/test_lasso.jl
+++ test/test_lasso.jl
@@ -9,14 +9,19 @@ x0 = zeros(n)
 x_star = [-3.877278911564627e-01, 0, 0, 2.174149659863943e-02, 6.168435374149660e-01]
 tol = 1e-9
 maxit = 10000
-verbose = 1
+verbose = 0
 tol_test = 1e-4
 
+@printf("Solving a small lasso instance (m = %d, n = %d)\n", m, n)
+
 @time x_ista,  it =  ista(A, b, g, x0, maxit, tol, verbose)
+@test it < maxit
 @test norm(x_ista-x_star, Inf)/norm(x_star, Inf) <= tol_test
 
 @time x_fista, it = fista(A, b, g, x0, maxit, tol, verbose)
+@test it < maxit
 @test norm(x_fista-x_star, Inf)/norm(x_star, Inf) <= tol_test
 
 @time x_zerofpr, it = zerofpr(A, b, g, x0, maxit, tol, verbose)
+@test it < maxit
 @test norm(x_zerofpr-x_star, Inf)/norm(x_star, Inf) <= tol_test
