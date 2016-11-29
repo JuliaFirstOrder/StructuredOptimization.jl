bdfb2af0b72e64a384badfe907153f5e41f2d0d0
nantonel <nantonel@esat.kuleuven.be>
2016-11-29 13:46:46 +0100

    background removal done

--- demo/matrix_decomposition.jl
+++ demo/matrix_decomposition.jl
@@ -3,11 +3,13 @@ using ProximalOperators
 using Images, ImageView
 
 srand(123)
-N = 30
+N = 20
 Nframes = 2621 #number of frames
 frames =  randperm(Nframes)[1:N]
 n,m = 160,120 #frame size
 
+# data from http://research.microsoft.com/en-us/um/people/jckrumm/wallflower/testimages.htm
+# Bootstrapt.zip
 R,G,B = zeros(Float64,n,m,N),zeros(Float64,n,m,N),zeros(Float64,n,m,N)
 
 for f in eachindex(frames)
@@ -22,30 +24,40 @@ end
 
 RGB = [reshape(R,n*m,N); reshape(G,n*m,N); reshape(B,n*m,N)]'
 
+
 Frg = zeros(Float64,N*n*m,3)
 Bkg = zeros(Float64,N,3*n*m)
-L = X-> reshape(X[1],N,3*n*m) +X[2]
+L = X-> reshape(X[1],N,3*n*m)+X[2]
 Ladj = Y-> [reshape(Y,N*n*m,3),Y]
 
-#norm(vecdot(L([Frg,Bkg]),RGB)-vecdot([Frg,Bkg],Ladj(RGB))) #verify adjoint operator
+XX =[randn(size(Frg)),randn(size(Bkg))] 
+norm(vecdot(L(XX),RGB)-vecdot(XX,Ladj(RGB))) #verify adjoint operator
 
 g = SeparableSum([NormL21(0.1,2), IndBallRank(1)])
 
 @time X,  = solve(L,Ladj, RGB, g, [Frg,Bkg], ZeroFPR(verbose = 2))
 
 Frg = copy(X[1])
+Frg[Frg.!=0] = Frg[Frg.!=0]+reshape(X[2],N*m*n*3)[Frg.!=0]
 Frg[repmat(sum(Frg,2).==0,1,3)] = 1 #put white in null pixels
-Frg[Frg.<0] = abs(Frg[Frg.<0])
 Frg = reshape(Frg,N,3*n*m)
-Bkg = X[2][1,:]
 
 f = 1	
 a = @sprintf("%5.5i",frames[f])
 img = data(load("bootstrap/b$a.bmp"))
-Bkgim = shareproperties(img, reshape(Bkg,n,m,3))
+Bkgim = shareproperties(img, reshape(X[2][1,:],n,m,3))
 Frgf = reshape(Frg[f,:],n,m,3)
 
 Frgim = shareproperties(img, Frgf)
 ImageView.view(img,xy=["y","x"])
 ImageView.view(Frgim,xy=["y","x"])
 ImageView.view(Bkgim,xy=["y","x"])
+
+
+
+
+
+
+
+
+
