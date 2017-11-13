85f9dc396c01762cb003fadc50eeb198d1cf65d0
nantonel <nantonel@esat.kuleuven.be>
2017-11-13 15:57:56 +0100

    improvements to DNN demo

--- demos/DNN.jl
+++ demos/DNN.jl
@@ -1,34 +1,42 @@
 using RegLS
 
 srand(123)
-Na,Nb = 300,300
-rho = 10.
-a = vcat([ 10.*[ cos(theta)  sin(theta)] for theta in linspace(0,2*pi,Na) ]...)   
-a .+= randn(size(a))
+Na,Nb = 700,700
+rhoa = 5.
+a = vcat([ rhoa.*[ cos(theta)  sin(theta)] for theta in linspace(0,2*pi,Na) ]...)   
+a .+= 1.5.*randn(size(a))
 b = [zeros(div(Nb,2))  zeros(div(Nb,2))]
-b = [b; vcat([ 20.*[ cos(theta)  sin(theta)] for theta in linspace(0,2*pi,div(Nb,2)) ]...)]   
+rhob = 8.
+b = [b; vcat([ rhob.*[ cos(theta)  sin(theta)] for theta in linspace(0,2*pi,div(Nb,2)) ]...)]   
 b .+= randn(size(b))
 bi = [ones(Bool,Na);zeros(Bool,Nb)]
 
-gamma = 1.
-L = 20
-W1 = Variable(0.5*randn(2,L))
-S1 = Sigmoid((Na+Nb,L),gamma) 
-W2 = Variable(0.5*randn(L,L))
-S2 = Sigmoid((Na+Nb,L),gamma) 
-W3 = Variable(0.5*randn(L,1))
+A = [[a[:,1];b[:,1]] [a[:,2];b[:,2]]]
+#preprocess data
+A .-= mean(A)
+A ./= sqrt.(var(A,1))
+
+L = 7  #inner layers
+W1 = Variable(sqrt(2*L)*randn(2,L))
+S1 = Sigmoid((Na+Nb,L)) 
+W2 = Variable(sqrt(L*L)*randn(L,L))
+S2 = Sigmoid((Na+Nb,L)) 
+W3 = Variable(sqrt(L)*randn(L,1))
 S3 = Sigmoid((Na+Nb,1)) 
 
-A = [[a[:,1];b[:,1]] [a[:,2];b[:,2]]]
-lambda = 1e-6
-lambda1 = lambda*length(~W1)
-lambda2 = lambda*length(~W2)
-lambda3 = lambda*length(~W3)
+lambda = 8e-5
+lambda1 = lambda*sqrt(2*L)
+lambda2 = lambda*sqrt(L*L)
+lambda3 = lambda*sqrt(L)
 
-slv = @minimize crossentropy(  S3*(S2*((S1*(A*W1))*W2)*W3)  ,bi)+lambda1*norm(W1,1)+lambda2*norm(W2,1)+lambda3*norm(W3,1) with ZeroFPR(tol = 1e-4, maxit = 2000) 
+slv = ZeroFPR(tol = 1e-4, maxit = 20000)
+reg = lambda1*norm(W1,1)+lambda2*norm(W2,1)+lambda3*norm(W3,1)
 
-xx = linspace(-30,30,200)
-yy = linspace(-30,30,200)
+slv = @minimize crossentropy(  S3*(S2*((S1*(A*W1))*W2)*W3)  ,bi)+reg with slv 
+println(slv)
+
+xx = linspace(-3,3,200)
+yy = linspace(-3,3,200)
 S1 = Sigmoid((2,L)) 
 S2 = Sigmoid((2,L)) 
 S3 = Sigmoid((2,1)) 
@@ -36,7 +44,8 @@ S3 = Sigmoid((2,1))
 using PyPlot
 figure()
 pcolormesh(xx,yy,[sum( S3*(S2*((S1*([xi yi]*~W1))*~W2)*~W3) )  for xi in xx, yi in yy]', cmap = "Oranges" ) 
-plot(a[:,1],a[:,2], "r*")
-plot(b[:,1],b[:,2], "ko")
+contour(xx,yy,[sum( S3*(S2*((S1*([xi yi]*~W1))*~W2)*~W3) )  for xi in xx, yi in yy]')
+plot(A[Na+1:end,1],A[Na+1:end,2], "ko")
+plot(A[1:Na,1],A[1:Na,2], "r*")
 
 
